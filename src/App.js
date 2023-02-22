8517d05b3fbe2227bfccbaf18a006e50ab8e0572
Abdelrahman Mohamed <abdelrahman181181@yahoo.com>
2023-02-22 21:45:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <div>
+            <Navbar/>
+            <Hero/>
+            <Card
+               image="https://www.athletespeakers.com/storage/celebrities/1627410490_Katie-Zaferes-Banner-1.jpg" 
+               rating="5.0" 
+               reviewCount = {6} 
+               country="USA" 
+               title="Life Lessons with Katie Zaferes"
+               price={136}
+               />
+        </div>
+    )
 }
-
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Navbar(){
+    return(
+        <nav>
+            <img className="nav-logo" src='./images/airbnb.png' alt='airbnb'/>
+        </nav>
+    )
+}
+export default Navbar;
