141241d84fd4259bfc297a6305d3b2e827f5d35f
Abdelrahman Mohamed <abdelrahman181181@yahoo.com>
2023-02-27 19:50:18 +0200

    mapping over data to render cards with .map()

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import Data from './data'
 
 function App(){
+    const Cards = Data.map(
+        function(x){
+            return <Card key = {x.id}
+                         image={x.coverImg}
+                         rating={x.stats.rating}
+                         reviewCount={x.stats.reviewCount}
+                         location={x.location}
+                         title={x.title}
+                         price={x.price}
+                    />
+        }
+    )
     return(
         <div>
             <Navbar/>
             <Hero/>
-            <Card
+            {/* <Card
                image="https://www.athletespeakers.com/storage/celebrities/1627410490_Katie-Zaferes-Banner-1.jpg" 
                rating="5.0" 
                reviewCount = {6} 
-               country="USA" 
+               location="USA" 
                title="Life Lessons with Katie Zaferes"
                price={136}
-               />
+               /> */}
+               {Cards}
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,15 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={props.image} className="card--image" alt="katie-zaferes"/>
+            <img src={props.image} className="card--image" alt="pic"/>
             <div className="card--stats">
                 <img src="https://www.pngkey.com/png/full/57-576538_pink-star-clip-art-at-clker-pink-star.png" alt="star" className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
